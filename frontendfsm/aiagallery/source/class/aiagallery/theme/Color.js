4131b671b374f7e49bbce168d2770ec7ff472f7f
Chris_Giarla <chris_giarla@student.uml.edu>
2011-12-06 14:42:26 -0500

    Made color changes. Blue and Orange scheme to match scratch

--- frontendfsm/aiagallery/source/class/aiagallery/theme/Color.js
+++ frontendfsm/aiagallery/source/class/aiagallery/theme/Color.js
@@ -14,5 +14,49 @@ qx.Theme.define("aiagallery.theme.Color",
 
   colors :
   {
+      // Top of screen, blue
+      "background-application" : "#6192CD",
+      // Find Apps background, orange
+      "background-pane" : "#FAA41D",
+      // Unknown
+      "background-medium" : "#FAA41D",
+      // Unknown
+      "background-splitpane" : "#FAA41D",
+      // Unknown
+      "background-tip" : "#FAA41D",
+      // Unknown
+      "background-odd" : "#FAA41D",
+      // Unknown
+      "htmlarea-background" : "#6192CD",
+      // Unkown
+      "border-main" : "#FAA41D",
+    	// Unkown
+    	"border-main" : "#FAA41D",
+    	// Unknown
+    	"menu-separator-top" : "#FAA41D",
+	// Unknown
+    	"menu-separator-bottom" : "#FAA41D",
+	// Unknown
+    	"border-pane" : "#6192CD",
+	// Unknown
+    	"border-button" : "#FAA41D",
+	// My Stuff background and Management table background
+    	"table-pane" : "#6192CD",
+
+	// GRADIENTS
+	"tabview-start" : "#FAA41D",
+	"tabview-end" : "#6192CD",
+	
+	// Inactive tabs	
+	"tabview-inactive" : "#ACEC01",
+   	"tabview-inactive-start" : "#ACEC01",
+    	"tabview-inactive-end" : "#ACEC01",
+
+    	"table-header-start" : "#FAA41D",
+    	"table-header-end" : "#FAA41D"
+
+	
+      
+      
   }
 });
